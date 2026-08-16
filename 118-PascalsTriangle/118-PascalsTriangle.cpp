4df23764c1470b183ff6991c63fe1d9bf4755736
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:22:59 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/16/2026, 9:23:01 PM
+class Solution {
+public:
+    vector<int> findRow(int n){
+        vector<int> ans;
+        int prod = 1;
+        for(int i = 1 ; i <= n ; i++){
+           ans.push_back(prod);
+           prod = prod*(n-i)/i; 
+        }
+        return ans;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans ;
+         for(int i = 1; i <= numRows ;i++){
+            ans.push_back(findRow(i));
+         }
+         return ans;
+    }
+};
