94cb3eb1968cace65c5cfb139143168916356119
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:13:35 +0530

    Create 2243-CheckIfAllAsAppearsBeforeAllBs

---
+++ 2243-CheckIfAllAsAppearsBeforeAllBs/2243-CheckIfAllAsAppearsBeforeAllBs.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/16/2026, 9:13:37 PM
+class Solution {
+public:
+    bool checkString(string s) {
+        
+        for(int i = 0 ; i < s.size() - 1 ; i++){
+            if(s[i] == 'a' && (s[i+1] == 'a' || s[i+1] == 'b' )){
+                continue;
+            }else if(s[i] == 'b' && s[i+1] =='a'){
+                return false;
+            }
+        }
+        return true;
+    }
+};
