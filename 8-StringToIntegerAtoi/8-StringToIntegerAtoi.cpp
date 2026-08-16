534e5407ed28c1ac5b7749f20adfd21936a6dc2b
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:24:34 +0530

    Create 8-StringToIntegerAtoi

---
+++ 8-StringToIntegerAtoi/8-StringToIntegerAtoi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/16/2026, 9:24:36 PM
+class Solution {
+public:
+    int myAtoi(string s) {
+
+        int i = 0;
+        int sign = 1;
+        long long ans = 0;
+        while(i < s.size() && s[i] == ' '){
+            i++;
+        }
+        if(i < s.size() && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '-'){
+             sign = -1;
+          
+            }
+             i++;
+        }
+        while(i < s.size() && isdigit(s[i])){
+            ans = ans*10 + (s[i] - '0');
+            if(sign*ans > INT_MAX){
+                return INT_MAX;
+            }
+            if(sign*ans < INT_MIN){
+                return INT_MIN;
+            }
+            i++;
+        }
+        return sign*ans;
+    }
+};
