bd86d1c2679adb1e6c3576170a5a339f03552527
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:20:18 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.cpp
@@ -0,0 +1,32 @@
+// Last updated: 8/16/2026, 9:20:20 PM
+class Solution {
+public:
+  bool isVowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    }
+
+    string reverseVowels(string s) {
+        
+        int left = 0;
+        int right = s.size()-1;
+        
+            while (left < right) {
+            if (!isVowel(s[left])) {
+                left++;
+            }
+            else if (!isVowel(s[right])) {
+                right--;
+            }
+            else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+ return s;
+        }
+               
+
+    
+};
