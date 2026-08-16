f36e03f176432e8a41354396256f4a4b2dc348f0
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:19:23 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/16/2026, 9:19:25 PM
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.rbegin() , nums.rend());
+        int sum = 0;
+        for(int i = 0 ; i < nums.size() ; i+=2){
+                sum += nums[i+1];
+        }
+
+        return sum;
+    }
+};
