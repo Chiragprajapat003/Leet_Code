b32e5402eda8e77583678a9e905f06d53929f2b5
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:21:07 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/16/2026, 9:21:10 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+
+        node->val = temp->val;
+        node->next = temp->next;
+
+        delete temp;
+    }
+};
