156554d67e9f32c0f9dac8e2d35bf817cd7f71d9
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:20:58 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/16/2026, 9:21:00 PM
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0) return false;
+
+        while(n % 2 == 0) n /= 2;
+        while(n % 3 == 0) n /= 3;
+        while(n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+};
