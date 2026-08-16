275823b6d9f6819e740fa997c11e030c66fc7337
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:24:03 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/16/2026, 9:24:06 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       if(list1 == NULL || list2 == NULL) {
+        return list1 == NULL ? list2 : list1;
+       }
+
+     if(list1->val < list2->val){
+        list1->next = mergeTwoLists(list1->next,list2);
+        return list1;
+     }else{
+        list2->next = mergeTwoLists(list1,list2->next);
+         return list2;
+     }
+     
+    }
+};
