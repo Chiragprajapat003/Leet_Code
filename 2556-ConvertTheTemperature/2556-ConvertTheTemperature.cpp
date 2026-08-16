be207fc7865570ed4c4e9d3ce2fd3d377b13c727
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-16 21:12:28 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.cpp
@@ -0,0 +1,10 @@
+// Last updated: 8/16/2026, 9:12:31 PM
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans(2);
+        ans[0] = celsius+273.15;
+        ans[01] = celsius * 1.80 + 32.00;
+        return ans;
+    }
+};
