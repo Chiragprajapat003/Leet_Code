91578809e5dd5ae3822a86f357e668254ac36e25
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-08-25 22:25:05 +0530

    Create 498-DiagonalTraverse

---
+++ 498-DiagonalTraverse/498-DiagonalTraverse.cpp
@@ -0,0 +1,35 @@
+// Last updated: 8/25/2026, 10:25:04 PM
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+        
+
+        int m = mat.size();
+        int n = mat[0].size();
+        int row = 0; 
+        int col = 0;
+
+        for(int i = 0 ; i < m*n ; i++){
+            ans.push_back(mat[row][col]);
+
+            if((row+col)%2 == 0){
+                if(col == n-1){
+                    row++;
+                }else if(row == 0){
+                    col++;
+                }else{ row-- ; col++;}
+            }else{
+                if(row == m-1){
+                    col++;
+                }else if(col == 0){
+                    row++;
+                }else{
+                    row++; col--;
+                }
+            }
+        }
+           
+        return ans;
+    }
+};
