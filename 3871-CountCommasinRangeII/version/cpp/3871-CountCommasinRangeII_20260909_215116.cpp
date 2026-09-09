4758d561ca95131e8bfea009472d80c512bd540c
Chirag prajapat <chirag.prajapat.cg@gmail.com>
2026-09-09 21:51:17 +0530

    Create 3871-CountCommasinRangeII

---
+++ 3871-CountCommasinRangeII/version/cpp/3871-CountCommasinRangeII_20260909_215116.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/9/2026, 9:51:16 PM
+1class Solution {
+2public:
+3    long long countCommas(long long n) {
+4        long long count = 0;
+5
+6        if (n >= 1000)
+7            count += n - 999;
+8
+9        if (n >= 1000000)
+10            count += n - 999999;
+11
+12        if (n >= 1000000000)
+13            count += n - 999999999;
+14
+15        if (n >= 1000000000000)
+16            count += n - 999999999999;
+17
+18        if (n >= 1000000000000000)
+19            count += n - 999999999999999;
+20
+21        return count;
+22    }
+23};
